f96b8c2d24c2e0fc5186251a9910bf5665c78c46
Dmitry <ganovichev@gmail.com>
2024-10-07 13:24:43 +0700

    dev_07_10_2024_1

--- resources/js/Layouts/AuthenticatedStudentLayout.vue
+++ resources/js/Layouts/AuthenticatedStudentLayout.vue
@@ -30,6 +30,14 @@ const showingNavigationDropdown = ref(false);
                                 </NavLink>
                             </div>
                             <div class="space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <a
+                                    class='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+                                    target="_blank"
+                                    href="https://textai.su/">
+                                    Уроки с нейронкой
+                                </a>
+                            </div>
+                            <div class="space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink :href="route('logout')">
                                     Выход
                                 </NavLink>
